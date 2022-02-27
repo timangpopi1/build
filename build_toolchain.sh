3bc26aa6c6b63694711a972e9342517986b92c3d
fadlyas07 <mhmmdfdlyas@gmail.com>
2022-02-27 18:06:49 +0700

    initial build scripts
    Change-Id: Ib88e8020eebee478ad677c91bb82c8334d982389

---
+++ build_toolchain.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+DIR="$(pwd ...)"
+
+branch="release/12.x"
+
+git clone -b "$branch" https://github.com/llvm/llvm-project --depth=1
+
+JobsTotal="$(($(nproc)*4))"
+./build-llvm.py \
+    --clang-vendor "greenforce" \
+    --defines "LLVM_PARALLEL_COMPILE_JOBS=$JobsTotal LLVM_PARALLEL_LINK_JOBS=$JobsTotal CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3 LLVM_USE_LINKER=lld LLVM_ENABLE_LLD=ON" \
+    --projects "clang;lld;polly;compiler-rt" \
+    --incremental \
+    --no-update \
+    --no-ccache \
+    --targets "ARM;AArch64" \
+    --pgo kernel-defconfig \
+    --lto full \
+    --branch "$branch"
+
+./build-binutils.py --targets arm aarch64
+
+# Remove unused products
+rm -fr install/include
+rm -f install/lib/*.a install/lib/*.la
+
+# Strip remaining products
+for f in $(find install -type f -exec file {} \; | grep 'not stripped' | awk '{print $1}'); do
+	strip -s "${f: : -1}"
+done
+
+# Set executable rpaths so setting LD_LIBRARY_PATH isn't necessary
+for bin in $(find install -mindepth 2 -maxdepth 3 -type f -exec file {} \; | grep 'ELF .* interpreter' | awk '{print $1}'); do
+	# Remove last character from file output (':')
+	bin="${bin: : -1}"
+
+	echo "$bin"
+	patchelf --set-rpath "$DIR/install/lib" "$bin"
+done
+
+rel_date="$(date '+%Y%m%d')" # ISO 8601 format
+clang_version="$(install/bin/clang --version | head -n1 | cut -d' ' -f4)"
+files="clang-$clang_version-$rel_date.tar.gz"
+tar -czvf "$files" install/*
+rclone copy "$files" fadlyas07:drive -P
